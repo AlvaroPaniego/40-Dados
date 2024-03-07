3d3559003e10b876ecf327593a5b904a0baa6e20
Alvaro Paniego Fernandez <alvaro.paniego@JUANXXIII.net>
2024-03-07 19:54:43 +0100

    Seguir con el combobox

--- dados_40/lib/principal.dart
+++ dados_40/lib/principal.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/flutter_spinbox.dart';
 
-class Principal extends StatelessWidget {
+class Principal extends StatefulWidget {
   const Principal({super.key});
 
+  @override
+  State<Principal> createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+  List<String> damageList = ["1", "2", "3", "d6", "d3"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +21,56 @@ class Principal extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         crossAxisCount: 2,
-        children: const <Widget>[
-          Row(
-            children: [
-              Text("Ataques"),
-            ],
-          )
-          // input("Fuerza"),
-          // input("Penetracion"),
-          // input("Daño"),
-          // input("Resistencia"),
-          // input("Salvacion"),
-          // input("Heridas")
+        children: <Widget>[
+          dataInputs("Ataques", 100.0),
+          dataInputs("Fuerza", 25.0),
+          dataInputs("Penetración", 5.0),
+          dataInputDamage("Daño"),
+          dataInputs("Resistencia", 20.0),
+          dataInputs("Salvación", 2.0),
         ],
       ),
     );
   }
 
+  dataInputs(text, max) {
+    return Container(
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          color: Colors.blueGrey),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [Text(text), SpinBox(min: 1, max: max)],
+      ),
+    );
+  }
+
+  dataInputDamage(text) {
+    var selectedItem = damageList.first;
+    return Container(
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          color: Colors.blueGrey),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(text),
+          DropdownButton<String>(
+            value: selectedItem,
+            items: damageList.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                selectedItem = value!;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
