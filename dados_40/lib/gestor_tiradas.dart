dde2243f5511d629e8bc25ea63b9765de07f61fd
Alvar789 <156839823+AlvaroPaniego@users.noreply.github.com>
2024-03-08 13:44:28 +0100

    Funcionalidad basica para tirar dados terminada

---
+++ dados_40/lib/gestor_tiradas.dart
@@ -0,0 +1,130 @@
+import 'dart:math';
+
+class GestorTiradas{
+  Random random = Random();
+  int calculateHits(int bs, int attacks){
+    int hits = 0;
+    int dice;
+    for (var i = 0; i < attacks; i++) {
+      dice = random.nextInt(6) + 1;
+      if(dice >= bs){
+        hits++;
+      }
+    }
+    return hits;
+  }
+
+  int calculateWounds(int hits, int strength, int toughness){
+    int wounds = 0;
+    for (var i = 0; i < hits; i++) {
+      if(strength*2 >= toughness){
+        if(wounds2()){
+          wounds++;
+        }
+      }else if(strength > toughness){
+        if(wounds3()){
+          wounds++;
+        }
+      }else if(strength == toughness){
+        if(wounds4()){
+          wounds++;
+        }
+      }else if(strength < toughness){
+        if(wounds5()){
+          wounds++;
+        }
+      }else if(strength <= toughness*2){
+        if(wounds6()){
+          wounds++;
+        }
+      }
+    }
+    return wounds;
+  }
+
+  int calculateDamage(String damage, int wounds, int ap, int invul, int save){
+    int totalDamage = 0;
+    int dice;
+    int actuallyWounds = 0;
+    int finalSave = save + ap;
+    for(int i = 0; i < wounds; i++){
+      dice = random.nextInt(6)+1;
+      if(dice <= finalSave){
+        totalDamage = applyDamage(damage, totalDamage);
+        actuallyWounds++;
+      }
+    }
+    if(invul != 0 && invul != 1) {
+      totalDamage = 0;
+      for(int i = 0; i < actuallyWounds; i++){
+        dice = random.nextInt(6)+1;
+        if(dice <= invul){
+          totalDamage = applyDamage(damage, totalDamage);
+        }
+      }
+    }
+    return totalDamage;
+  }
+
+  int applyDamage(String damage, int totalDamage){
+    switch(damage){
+      case "1":
+        totalDamage++;
+        break;
+      case "2":
+        totalDamage+=2;
+        break;
+      case "3":
+        totalDamage+=3;
+        break;
+      case "d6":
+        totalDamage+=random.nextInt(6)+1;
+        break;
+      case "d3":
+        totalDamage+=random.nextInt(3)+1;
+        break;
+    }
+    return totalDamage;
+  }
+
+  bool wounds2(){
+    int dice = random.nextInt(6) + 1;
+    bool hasWounded = false;
+    if(dice >= 2){
+      hasWounded = true;
+    }
+    return hasWounded;
+  }
+  bool wounds3(){
+    int dice = random.nextInt(6) + 1;
+    bool hasWounded = false;
+    if(dice >= 3){
+      hasWounded = true;
+    }
+    return hasWounded;
+  }
+  bool wounds4(){
+    int dice = random.nextInt(6) + 1;
+    bool hasWounded = false;
+    if(dice >= 4){
+      hasWounded = true;
+    }
+    return hasWounded;
+  }
+  bool wounds5(){
+    int dice = random.nextInt(6) + 1;
+    bool hasWounded = false;
+    if(dice >= 5){
+      hasWounded = true;
+    }
+    return hasWounded;
+  }
+  bool wounds6(){
+    int dice = random.nextInt(6) + 1;
+    bool hasWounded = false;
+    if(dice >= 6){
+      hasWounded = true;
+    }
+    return hasWounded;
+  }
+}
--- dados_40/lib/principal.dart
+++ dados_40/lib/principal.dart
@@ -1,5 +1,8 @@
+import 'package:dados_40/gestor_tiradas.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinbox/flutter_spinbox.dart';
+
+List<String> damageList = ["1", "2", "3", "d6", "d3"];
 
 class Principal extends StatefulWidget {
   const Principal({super.key});
@@ -9,67 +12,157 @@ class Principal extends StatefulWidget {
 }
 
 class _PrincipalState extends State<Principal> {
-  List<String> damageList = ["1", "2", "3", "d6", "d3"];
+  String selectedItem = damageList.first;
+  String result = "", damage = damageList.first;
+  int bs = 2, attacks = 1, strength = 1, toughness = 1, save = 2, ap = 0, invul = 0;
+  GestorTiradas gt = GestorTiradas();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("40 Dados"),
       ),
-      body: GridView.count(
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          dataInputs("Ataques", 100.0),
-          dataInputs("Fuerza", 25.0),
-          dataInputs("Penetración", 5.0),
-          dataInputDamage("Daño"),
-          dataInputs("Resistencia", 20.0),
-          dataInputs("Salvación", 2.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Table(
+            children: [
+              TableRow(children: <Widget>[
+                dataInputs("BS", 6.0, 2.0),
+                dataInputs("Ataques", 100.0, 1.0),
+              ]),
+              TableRow(children: <Widget>[
+                dataInputs("Fuerza", 25.0, 1.0),
+                dataInputs("Penetración", 5.0, 0.0),
+              ]),
+              TableRow(children: <Widget>[
+                dataInputDamage("Daño"),
+                dataInputs("Resistencia", 20.0, 1.0),
+              ]),
+              TableRow(children: <Widget>[
+                dataInputs("Salvación", 7.0, 2.0),
+                dataInputs("Salvación invulnerable", 6.0, 0.0),
+              ]),
+            ],
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                result = updateResult(bs, attacks, strength, toughness, save, invul, ap, damage);
+              });
+            },
+            child: Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.grey),
+                padding: const EdgeInsets.all(15.0),
+                child: const Text(
+                  "¡Tirar dados!",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  color: Colors.blueGrey),
+              padding: const EdgeInsets.all(15.0),
+              margin: const EdgeInsets.all(8.0),
+              child: Text(result)
+          )
         ],
       ),
     );
   }
 
-  dataInputs(text, max) {
+  dataInputs(String text, double max, double min) {
     return Container(
+      width: MediaQuery.of(context).size.width,
       decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(15.0)),
           color: Colors.blueGrey),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [Text(text), SpinBox(min: 1, max: max)],
-      ),
-    );
-  }
-
-  dataInputDamage(text) {
-    var selectedItem = damageList.first;
-    return Container(
-      decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(15.0)),
-          color: Colors.blueGrey),
+      padding: const EdgeInsets.all(15.0),
+      margin: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text(text),
-          DropdownButton<String>(
-            value: selectedItem,
-            items: damageList.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
+          SpinBox(
+            min: min,
+            max: max,
+            value: min,
+            keyboardType: TextInputType.none,
             onChanged: (value) {
               setState(() {
-                selectedItem = value!;
+                switch(text){
+                  case "BS":
+                    bs = value.toInt();
+                    break;
+                  case "Ataques":
+                    attacks = value.toInt();
+                    break;
+                  case "Fuerza":
+                    strength = value.toInt();
+                    break;
+                  case "Penetración":
+                    ap = value.toInt();
+                    break;
+                  case "Resistencia":
+                    toughness = value.toInt();
+                    break;
+                  case "Salvación":
+
+                    save = value.toInt();
+                    break;
+                  case "Salvación invulnerable":
+                    invul = value.toInt();
+                    break;
+                }
               });
             },
-          ),
+          )
         ],
+      ),
+    );
+  }
+
+  String updateResult(int bs, int attacks, int strength, int toughness, int save, int invul, int ap, String damage){
+    int hits = gt.calculateHits(bs, attacks);
+    int wounds = gt.calculateWounds(hits, strength, toughness);
+    int totalDamage = gt.calculateDamage(damage, wounds, ap, invul, save);
+    return "Han impactado $hits\nHan herido $wounds\nLa unidad enemiga sufre $wounds heridas de $damage de daño\n($totalDamage)";
+  }
+
+  dataInputDamage(text) {
+    return TableCell(
+      verticalAlignment: TableCellVerticalAlignment.fill,
+      child: Container(
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            color: Colors.blueGrey),
+        margin: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(text),
+            DropdownButton<String>(
+              value: selectedItem,
+              icon: const Icon(Icons.arrow_downward),
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  selectedItem = value!;
+                  damage = value;
+                });
+              },
+              items: damageList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
